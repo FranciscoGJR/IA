708e00f1e3f446590952e11afc8ea60fcacd56a3
Igor Augusto <igoraugusto1029@gmail.com>
2024-04-26 23:56:39 +0000

    adiciona estudo de parametros

---
+++ best_model_helper.py
@@ -0,0 +1,12 @@
+import json
+
+with open('./modelos/models.json') as f:
+    data = json.load(f)
+
+# ordena modelos por melhor acurácia
+
+data = sorted(data, key=lambda x: x['validation_errors'][-1]["error"])
+
+for item in data:
+    acuracia = (1 - item['validation_errors'][-1]["error"]) * 100
+    print(item['model_name'], f'{acuracia:.2f}%')
